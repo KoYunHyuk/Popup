8762ac88b7ad10ada2ba02ec34e7675a2371329f
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2021-04-19 12:47:03 +0900

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import Modal from "./Modal"
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  function toggle() {
+    if(visible === true) setVisible(false);
+    else setVisible(true);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button onClick={toggle}>button</button>
+
+      <Modal visible={visible}>
+        Hello<br></br>
+        <button onClick={toggle}>button</button>
+      </Modal>
     </div>
-  );
+  )
 }
 
 export default App;
